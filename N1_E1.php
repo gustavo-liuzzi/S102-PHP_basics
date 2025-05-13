36860e3ca4d0db7ec53d869d7182a7071a66f80e
Gustavo A. Liuzzi <gustavo.liuzzi@gmail.com>
2025-05-13 13:41:39 +0200

    Primer ejercicio del nivel 1

---
+++ N1_E1.php
@@ -0,0 +1,16 @@
+<?php
+
+$entero = 100;
+$double = 34.84738475;
+$cadena = "Hola, mundo";
+$booleano = true;
+
+echo "$entero<br>";
+echo "$double<br>";
+echo "$cadena<br>";
+echo "$booleano<br>";
+
+define("NOMBRE", "gustavo liuzzi");
+echo ucwords(NOMBRE);
+
+?>
